c8e7e64bdf86e89324bf0a79186f7b2835ab4f4d
Aditya <sp.aditya@gmail.com>
2018-07-17 14:42:08 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+
+ui = input("Enter your name: ")
+print(ui)
+print(type(ui))
+
+while True:
+    ui = input('enter y to stop: ')
+    if ui == 'y':
+        #exit()
+        break
+        
+print("End of program")

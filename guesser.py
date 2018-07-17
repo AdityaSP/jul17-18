0b62dfdce5290d8533dd90e7dd4943a830db3c7e
Aditya <sp.aditya@gmail.com>
2018-07-17 15:18:10 +0530

    Add files via upload

--- guesser.py
+++ guesser.py
@@ -1,17 +1,40 @@
+# show the number of guesses and also print the guesses
+
 import random
 r = random.randint(1,100)
 
 print("I have thought of a number. Its your turn to guess")
-
+guesses = []
 while True:
     guess = input('Enter your guess (1,100) :')
+    guesses.append(guess)
+    
     if int(guess) < r:
         print("Low")
     elif int(guess) > r:
         print("High")
     else:
         print("Bingo!! You guessed it right")
+        print("You took", len(guesses), "guesses")
+        print("These were your guesses", guesses)
+        
         break
+
+        
+#import random
+#r = random.randint(1,100)
+#
+#print("I have thought of a number. Its your turn to guess")
+#
+#while True:
+#    guess = input('Enter your guess (1,100) :')
+#    if int(guess) < r:
+#        print("Low")
+#    elif int(guess) > r:
+#        print("High")
+#    else:
+#        print("Bingo!! You guessed it right")
+#        break
 
 
 #generate a random number

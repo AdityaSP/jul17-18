963ee5281207a9e28f303c65e67610a244a71c0a
Aditya <sp.aditya@gmail.com>
2018-07-18 14:59:12 +0530

    Add files via upload

--- proj/core/bank.py
+++ proj/core/bank.py
@@ -1,5 +1,8 @@
+# Savings Account, Current Account
+# SA if the b < amount = Should not allow
+
 class Account():
-    def __init__(self, n, b=1000, t='S'):
+    def __init__(self, n, b, t):
         self.n = n
         self.b = b
         self.t = t
@@ -7,3 +10,16 @@ class Account():
         self.b += amount
     def debit(self, amount):
         self.b -= amount
+
+class SA(Account):
+    def __init__(self, n, b=1000):
+        Account.__init__(self, n, b, 'S')
+    def debit(self, amount):
+        if self.b >= amount:
+            self.b -= amount
+        else:
+            raise ValueError('Insufficient balance')
+
+class CA(Account):
+    def __init__(self, n, b=0):
+        Account.__init__(self, n, b, 'C')
--- proj/transactions.py
+++ proj/transactions.py
@@ -1,15 +1,30 @@
 import core.bank
+
+sa = core.bank.SA('Aditya')
+print(sa.n, sa.b, sa.t)
+sa.credit(100)
+print(sa.n, sa.b, sa.t)
+sa.debit(2000)
+print(sa.n, sa.b, sa.t)
+
+
+ca = core.bank.CA('ABC Ltd')
+print(ca.n, ca.b, ca.t)
+ca.credit(200)
+print(ca.n, ca.b, ca.t)
+
+
 # ac1 = core.bank.Account()
 # print(type(ac1))
 
-ac1 = core.bank.Account('Aditya')
-print(ac1.n, ac1.b, ac1.t)
-ac1.credit(10000)
-print(ac1.n, ac1.b, ac1.t)
-ac1.debit(100)
-print(ac1.n, ac1.b, ac1.t)
-
-ac2 = core.bank.Account('John')
-print(ac2.n, ac2.b, ac2.t)
-ac2.credit(1000)
-print(ac2.n, ac2.b, ac2.t)
+# ac1 = core.bank.Account('Aditya')
+# print(ac1.n, ac1.b, ac1.t)
+# ac1.credit(10000)
+# print(ac1.n, ac1.b, ac1.t)
+# ac1.debit(100)
+# print(ac1.n, ac1.b, ac1.t)
+#
+# ac2 = core.bank.Account('John')
+# print(ac2.n, ac2.b, ac2.t)
+# ac2.credit(1000)
+# print(ac2.n, ac2.b, ac2.t)

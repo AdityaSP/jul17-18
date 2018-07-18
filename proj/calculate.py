46e87f763de377bb3729542b643cdc58e1d404d6
Aditya <sp.aditya@gmail.com>
2018-07-18 12:03:39 +0530

    Add files via upload

---
+++ proj/calculate.py
@@ -0,0 +1,19 @@
+# len, type, int, str, id => builtins
+# random.randint(1,100) => standard library called random
+# mathops => user created module
+
+from utils import mathops
+
+print(mathops.add(3, 4))
+
+# from mathops import add
+# print(add(3,4))
+
+# def mul(x,y):
+#     return x*x * y*y
+#
+# from mathops import *
+# print(add(3,4))
+# print(sub(3,4))
+# print(mul(3,4))
+
---
+++ proj/calculatev2.py
@@ -0,0 +1,21 @@
+# import utils.mathops
+# print(utils.mathops.add(3,4))
+# print(utils.mathops.sub(3,4))
+
+# from utils import mathops
+# print(mathops.add(3,4))
+# print(mathops.sub(3,4))
+
+# from utils.mathops import add
+# print(add(3,4))
+
+# should not be used in prod
+# from utils.mathops import *
+# print(add(3,4))
+# print(sub(3,4))
+
+import utils.mathops as ops
+print(ops.add(3,4))
+print(ops.sub(3,4))
+
+
